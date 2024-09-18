205eb7bcb8fdadac9fc60d5a432977c32b92137f
Andres Moreno <hola@thegoodhackertv.com>
2024-09-18 22:04:14 +0200

    Update openvpn-install.sh
    ♻️  fixed labs_network login

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -863,7 +863,7 @@ ifconfig-pool-persist ipp.txt" >>/etc/openvpn/server.conf
 
   	#Check if network is defined for aditional route
  	LABS_NETWORK=${LABS_NETWORK:-n}
-  	if [[ $IPV6_SUPPORT != 'n' ]]; then
+  	if [[ $LABS_NETWORK != 'n' ]]; then
 		echo 'push "route $LABS_NETWORK 255.255.0.0"' >>/etc/openvpn/server.conf
 	fi
  
