2e3ace9e1403597257fddf85838e4c082d497f94
Andres Moreno <hola@thegoodhackertv.com>
2024-09-20 19:50:11 +0200

    Create create_client.sh

---
+++ create_client.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Verificar si se ha proporcionado un nombre de usuario
+if [ -z "$1" ]; then
+  echo "Uso: $0 <nombre_de_usuario>"
+  exit 1
+fi
+
+export MENU_OPTION="1"
+export CLIENT="$1"
+export PASS="1"
+./openvpn-install.sh
